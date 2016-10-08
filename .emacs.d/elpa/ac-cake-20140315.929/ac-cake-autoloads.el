0a5d70f005a56c83ec0e8bc8c5d19fb5abc6f41e
lanocci <kurn17.kkum@gmail.com>
2016-10-08 09:55:53 +0900

    delete unnecessary files

--- .emacs.d/elpa/ac-cake-20140315.929/ac-cake-autoloads.el
+++
@@ -1,15 +0,0 @@
-;;; ac-cake-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("ac-cake.el") (22510 30333 682951 0))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; ac-cake-autoloads.el ends here
--- .emacs.d/elpa/elscreen-1.4.6/elscreen-autoloads.el
+++
@@ -1,15 +0,0 @@
-;;; elscreen-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("elscreen.el") (22508 19275 736398 0))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; elscreen-autoloads.el ends here
