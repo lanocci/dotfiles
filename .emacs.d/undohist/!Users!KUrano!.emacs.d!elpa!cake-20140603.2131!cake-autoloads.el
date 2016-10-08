fcfb20239afff6425b8fa79eaef56baf12d63f46
lanocci <kurn17.kkum@gmail.com>
2016-10-08 09:55:06 +0900

    delete unnecessary files

--- .emacs.d/undohist/!Users!KUrano!.emacs.d!elpa!cake-20140603.2131!cake-autoloads.el
+++
@@ -1,25 +0,0 @@
-
-((digest . "79c96a73ce0be17f347d4bb5ed4e4d56") (undo-list (561 . 637) ("
-;;;### (autoloads nil nil (\"cake-pkg.el\") (22510 30333 239545 0))
-
-;;;***
-" . -561) (553 . 561) (145 . 242) (388 . 456) (382 . 383) (213 . 387) (193 . 194) (145 . 212) ("
-;;;### (autoloads nil \"cake\" \"cake.el\" (22510 30333 0 0))
-;;; Generated autoloads from cake.el
-
-(autoload 'cake \"cake\" \"\\
-CakePHP minor mode.
-
-\\(fn &optional ARG)\" t nil)
-
-(when (fboundp 'define-global-minor-mode) (define-global-minor-mode global-cake cake cake-maybe :group 'cake))
-
-(autoload 'cake-snippets-initialize \"cake\" \"\\
-
-
-\\(fn)\" nil nil)
-
-(eval-after-load 'yasnippet '(cake-snippets-initialize))
-
-;;;***
-" . -145) (t 22510 30333 0 0) (553 . 637) (145 . 242) (388 . 456) (382 . 383) (213 . 387) (193 . 194) (145 . 212) (t 22510 30333 0 0)))
--- .emacs.d/undohist/!Users!KUrano!projects!dotfiles!.emacs.d!init.el
+++
@@ -1,2 +0,0 @@
-
-((digest . "1a3e6c18e61482650fd63502780f78a2") (undo-list nil (3669 . 3675) nil undo-tree-canary))
