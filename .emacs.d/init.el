3015fd79044d790fbeefb1e77447eecc9dbf0362
lanocci <kurn17.kkum@gmail.com>
2016-10-08 17:12:45 +0900

    enable scss-mode

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -501,6 +501,23 @@
 (add-hook 'ruby-mode-hook 'ruby-mode-hooks)
 
 
+;; scss-mode
+(require 'scss-mode)
+(add-to-list 'auto-mode-alist '("\\.scss$" . scss-mode))
+ 
+;; インデント幅を2にする
+;; コンパイルは compass watchで行うので自動コンパイルをオフ
+(defun scss-custom ()
+  "scss-mode-hook"
+  (and
+   (set (make-local-variable 'css-indent-offset) 2)
+   (set (make-local-variable 'scss-compile-at-save) nil)
+   )
+  )
+(add-hook 'scss-mode-hook
+  '(lambda() (scss-custom)))
+
+
 ;; Makefileの種類を定義
 (defvar flymake-makefile-filenames
   '("Makefile" "makefile" "GNUmakefile")
