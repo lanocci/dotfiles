ad595f73b62f8af9658dc9ca9597a73515180355
lanocci <kurn17.kkum@gmail.com>
2016-10-09 15:39:30 +0900

    modify initialization error

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -29,7 +29,7 @@
 ;; インストールするパッケージのリスト
 (defvar my/packages
   '(
-    undohist undo-tree anything elscreen markdown-mode eruby-mode slim-mode wgrep web-mode flycheck helm
+    undohist undo-tree anything elscreen markdown-mode eruby-mode slim-mode wgrep web-mode flycheck helm nxml-mode auto-complete scss-mode flymake-css
    ))
 
 ;; リストのパッケージをインストール
@@ -55,23 +55,23 @@
 ;; elgetでリストの内容をインストール
 (defvar my/el-get-packages
   '(
-    howm egg init-loader
+    howm egg init-loader 
    ))
 (el-get 'sync my/el-get-packages)
 
 
 ;; load-path を追加する関数を定義
-(defun add-to-load-path (&rest paths)
-  (let (path)
-    (dolist (path paths paths)
-      (let ((default-directory
-              (expand-file-name (concat user-emacs-directory path))))
-        (add-to-list 'load-path default-directory)
-        (if (fboundp 'normal-top-level-add-subdirs-to-load-path)
-            (normal-top-level-add-subdirs-to-load-path))))))
+;; (defun add-to-load-path (&rest paths)
+;;   (let (path)
+;;     (dolist (path paths paths)
+;;       (let ((default-directory
+;;               (expand-file-name (concat user-emacs-directory path))))
+;;         (add-to-list 'load-path default-directory)
+;;         (if (fboundp 'normal-top-level-add-subdirs-to-load-path)
+;;             (normal-top-level-add-subdirs-to-load-path))))))
 
 ;; 引数のディレクトリとそのサブディレクトリをload-pathに追加
-(add-to-load-path "elisp" "conf" "public_repos")
+;; (add-to-load-path "elisp" "conf" "public_repos")
 
 (require 'egg)
 
